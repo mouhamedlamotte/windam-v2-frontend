8f0d0300276ff6956b3b53ad9b9541ae76855d69
mouhamedbaba <lamottelymouhamed@gmail.com>
2024-08-26 13:02:17 +0000

    fix : set debug to false

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 
-const DEBUG = true
+const DEBUG = false
 
 const baseURL =DEBUG ?  "http://localhost:8000/api"  : 'https://windam-backend.onrender.com/api'
 const baseWsUrl = DEBUG ? "ws://localhost:8000/ws" : "wss://windam-backend.onrender.com/ws"
