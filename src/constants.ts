8c36203b98769b446b6fd24f40c620b00fe25dd8
mouhamedbaba <lamottelymouhamed@gmail.com>
2024-08-26 15:14:35 +0000

    fix : make debug var dinamic

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,9 @@
+import { env } from "process"
 
-const DEBUG = true
+const DEBUG = env.NODE_ENV === "development"
+
+
+
 
 const baseURL =DEBUG ?  "http://localhost:8000/api"  : 'https://windam-backend.onrender.com/api'
 const baseWsUrl = DEBUG ? "ws://localhost:8000/ws" : "wss://windam-backend.onrender.com/ws"
